ff252c88d9664d9c2545fd1db855c967f7f62bf4
Jy7733 <101092273+Jy7733@users.noreply.github.com>
2023-12-04 21:11:42 +0900

    Update kvs.h

--- kvs_lab/kvs.h
+++ kvs_lab/kvs.h
@@ -5,8 +5,8 @@
 struct node {
 	char key[100];
 	char* value;
-	// char value[1000];
 	struct node* next;
+	// UT_hash_handle hh;
 };	
 typedef struct node node_t;
 
@@ -14,11 +14,21 @@ typedef struct node node_t;
 struct kvs{
 	struct node* db; // database
 	int items; // number of data 
-};
+}; 
 typedef struct kvs kvs_t; 
 
 
-kvs_t* open();
-int close(kvs_t* kvs); // free all memory space 
+
+kvs_t* kvs_open();
+int kvs_close(kvs_t* kvs); // free all memory space 
 int set(kvs_t* kvs, const char* key, const char* value); // return -1 if failed.
 char* get(kvs_t* kvs, const char* key); // return NULL if not found. 
+
+extern int is_recovery;
+
+//baseline 버전 snapshot,recovery
+void do_snapshot(kvs_t* kvs);
+void do_recovery(kvs_t* kvs);
+//custom 버전 snapshot, recovery
+void do_cust_snpshot(kvs_t* kvs);
+void do_cust_recovery(kvs_t* kvs);
