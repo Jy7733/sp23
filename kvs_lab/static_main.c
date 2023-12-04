88c00b2b71f942cf669929bf0874df1ab3562503
Jy7733 <101092273+Jy7733@users.noreply.github.com>
2023-12-04 21:13:09 +0900

    Update static_main.c

--- kvs_lab/static_main.c
+++ kvs_lab/static_main.c
@@ -2,58 +2,60 @@
 
 int main()
 {
-	kvs_t* kvs = open();
+	kvs_t* kvs = kvs_open();
 
 	if(!kvs) {
 		printf("Failed to open kvs\n");
 		return -1;
 	}
 
-	// workload execution  
-	
-	// 1) 	file read 
-	// 2) 	if set, insert (key, value) into kvs.
-	// 		if get, seach the key in kvs and return the value. 
-	//		Return -1 if the key is not found  
+	// FILE* file = fopen("cluster004.trc","r");
+	FILE* file = fopen("cluster000.trc","r");
+	// FILE* snpFile = fopen("kvs.img","w");
 
-	//query.dat 파일 열기
-	FILE* file = fopen("query.dat","r");
-	if (file == NULL) {
-		perror("open fail");
-		return -1;   //오류 발생 시 -1 return 
-	}
+	int line_size = 1024;
+	char* line = malloc(line_size * sizeof(char));
 
-	//answer.dat 파일 열기
-	FILE* fp = fopen("answer.dat","w");
-	if (file == NULL) {
-		perror("open fail");
-		return -1;   //오류 발생 시 -1 return 
-	}
-	
-	//qeury.dat 에서 한 줄 씩 읽어오기
-	char line[512];
-	while (fgets(line,sizeof(line),file)!=NULL) {
-		char cmd[5]; //set혹은 get
-		char k[100]; //게시물id
-		char v[100]; //사용자 id
+	int v_size = 512;
+	char* v = malloc(v_size * sizeof(char));
 
-		sscanf(line,"%[^,],%[^,],%s",cmd,k,v);
+	char cmd[5];
+	char k[300];
 
-		//cmd가 set이면 set함수 호출
-		if (strcmp(cmd,"set")==0) {
+
+	while(fgets(line,line_size,file)!=NULL) {
+		//line의 크기가 부족하다면, realloc으로 재할당
+		if(strlen(line) >= line_size -1){
+			line_size *=2;
+			line = realloc(line,line_size * sizeof(char));
+		}
+		//v의 크기가 부족하다면,realloc으로 재할당 
+		if(strlen(line) > v_size) {
+			v_size = strlen(line) +1;
+			v = realloc(v,v_size * sizeof(char));
+		}
+
+		sscanf(line,"%[^,],%[^,],%[^\n]",cmd,k,v);
+
+		if(strcmp(cmd,"set")==0) {
 			set(kvs,k,v);
+			do_snapshot(kvs);
+			// do_cust_snpshot(kvs);
 		}
-
-		//cmd가 get이면 get함수 호출
-		else if (strcmp(cmd,"get")==0) {
-			char* val = get(kvs,k);
-			fprintf(fp,"%s\n",val);
+		else if(strcmp(cmd,"get")==0) {
+			get(kvs,k);
 		}
 	}
+	// do_snapshot(kvs);
+	// do_cust_snpshot(kvs);
 	printf("kvs items : %d",kvs->items);
-	fclose(file);
-	fclose(fp);
 
-	close(kvs);
+	free(line);
+	free(v);
+	fclose(file);
+	// fclose(snpFile);
+	kvs_close(kvs);
+	
+
 	return 0;
 }
